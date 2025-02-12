b88b1b88dab6f523525dd9d26a8192fba2e779d1
SARA SOUHAIL <127333635+SaraSouhail@users.noreply.github.com>
2025-02-12 23:42:04 +0100

    Delete SaraSouhail-rails-garden-manager-2024-02-22 directory

--- SaraSouhail-rails-garden-manager-2024-02-22/README.md
+++
@@ -1,1 +0,0 @@
-Rails application used for the 4th deploy challenge of the [lewagon/assess](https://github.com/lewagon/assess) web certification.
--- SaraSouhail-rails-garden-manager-2024-02-22/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root to: "gardens#index"
-
-  resources :gardens, only: [:index, :show, :new, :create] do
-    resources :plants, only: [:create]
-  end
-  resources :plants, only: [:destroy]
-end
